8bba7e6f7fb8a5a82946de739fc44a89f6028583
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-03-20 19:20:44 -0300

    Add files via upload

---
+++ Query & Route params/index.js
@@ -0,0 +1,35 @@
+//importando o express
+const express = require('express');
+
+const server=express();
+
+
+// Query params = http://localhost:3000/teste?nome=Pedro
+
+
+server.get('/teste',(req,res)=>{
+    const nome = req.query.nome;
+    return res.json({mensagem:nome});
+})
+/*
+{
+  "mensagem": "Pedro"
+} 
+*/
+
+// Route Params = http://localhost:3000/teste/5
+server.get('/teste/:id',(req,res)=>{
+    const id = req.params.id;
+    // com desestruturação
+    // const {id} = req.params;
+    return res.json({mensagem:id});
+})
+/*
+{
+  "mensagem": "5"
+} 
+*/
+
+
+
+server.listen(3000);
